bda10ea6a1f9fce8dea873ed97076288c341e7c5
pkim <pdkim85@gmail.com>
2013-04-18 10:53:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+D3testing::Application.config.secret_token = '9efe487d6c910ee5d1d5319856ae3a3e634bc2f42c91417f99cd5b0762683f0543ad4acff7914a2ab0ecc3cc152b1a28b73075881836fb74ceb9e7b231277f88'
